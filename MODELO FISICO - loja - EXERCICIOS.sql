ce80ccf96051885ebb73ab6677a38f06ea7c296f
Jeferson Alan <cabello_10@live.com>
2024-08-29 16:30:30 -0300

    Update MODELO FISICO - loja - EXERCICIOS.sql

--- MODELO FISICO - loja - EXERCICIOS.sql
+++ MODELO FISICO - loja - EXERCICIOS.sql
@@ -14,10 +14,10 @@ SELECT curdate() ;
 INSERT INTO pedidos (id_cliente, data_pedido)
 	VALUES ('1', date_add(CURDATE(), INTERVAL 1 DAY));
     
--- ADICIONAR DIFERENCA ENTRE DUAS DATAS 
+-- CALCULAR DIFERENCA ENTRE DUAS DATAS 
 SELECT DATEDIFF('2024-08-01', CURDATE());
 
--- OBTER DATA  DOIS DIAS ANTES DE HOJE 
+-- OBTER DATA  DOIS DIAS ANTES DE HOJE DE UM PAGAMENTO
 SELECT DATE_SUB(CURDATE(), INTERVAL 2 DAY);
 
 -- INSERIR UM PAGAMENTO PARA UMA DATA ESPECIFICA
@@ -33,10 +33,16 @@ INSERT INTO pagamentos (data_pagamento, valor_pagamento, id_pedido)
 SELECT * FROM pedidos 
 	WHERE data_pedido = curdate() ;
     
+-- Qual função SQL retorna a data atual e hora atual?
+      SELECT curdate() AS data_atual, curtime() AS hora_atual;
+
 -- COMO ADICIONAR UM MES APOS A DATA ATUAL
 SELECT DATE_ADD(curdate(), INTERVAL 1 MONTH );
 
--- COMO ALTERAR A DATA DE NASCIMENTO DE UM CLIENTE PARA 5 ANOS ATRAS
+-- COMO ALTERAR A DATA DE NASCIMENTO DE UM CLIENTE PARA 15 ANOS ATRAS
 UPDATE clientes 
 	SET data_nasc = DATE_SUB(CURDATE(), INTERVAL 15 YEAR) 
 		WHERE id = 1 ;
+        
+-- Como obter a diferença entre a data de pagamento e a data do pedido?
+SELECT DATEDIFF('2024-08-01', '2024-07-25') AS dias_de_diferença;
